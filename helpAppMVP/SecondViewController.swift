26c852491667bc288ecf5c76a5ee4c4ffcf57bf6
Hunter Leeves <hunterleeves@me.com>
2020-06-19 18:38:32 -0500

    Initial Commit

---
+++ helpAppMVP/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  helpAppMVP
+//
+//  Created by Hunter Leeves on 6/19/20.
+//  Copyright © 2020 Hunter Leeves. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
